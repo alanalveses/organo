886a8077496237dd92bd532e2162dd83bc901750
Alan Alves <alan.alveses@gmail.com>
2024-01-18 22:56:40 -0300

    Criando componente "botao"

--- src/componentes/Formulario/index.js
+++ src/componentes/Formulario/index.js
@@ -1,3 +1,4 @@
+import Botao from '../Botao'
 import CampoTexto from '../CampoTexto'
 import ListaSuspensa from '../ListaSuspensa'
 import './Formulario.css'
@@ -20,6 +21,9 @@ const Formulario = () => {
             <CampoTexto label="Classe" placeholder='Digite seu classe'/>
             <CampoTexto label="Imagem" placeholder='Digite seu endereço da imagem'/>
             <ListaSuspensa label="Time" itens={times} />
+            <Botao > 
+                Criar card
+            </Botao >
             </form>
         </section>
     )
