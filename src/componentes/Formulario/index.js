228bd5fdc1825ac680f1aedc7cd3801117aed869
Alan Alves <alan.alveses@gmail.com>
2024-01-18 23:17:37 -0300

    Mudando o comportamento padrão do onSubmit e validação dos campos

--- src/componentes/Formulario/index.js
+++ src/componentes/Formulario/index.js
@@ -13,14 +13,19 @@ const Formulario = () => {
         'Suporte',
         'Soldado'
     ]
+
+    const aoSalvar = (evento) => {
+        evento.preventDefault()
+        console.log('Form foi enviado')
+    }
     return(
         <section className='formulario'>
-            <form>
+            <form onSubmit={aoSalvar}>
                 <h2>Preencha os dados para criar o card dos jogadores</h2>
-            <CampoTexto label="Nome" placeholder='Digite seu nome'/>
-            <CampoTexto label="Classe" placeholder='Digite seu classe'/>
+            <CampoTexto obrigatorio={true} label="Nome" placeholder='Digite seu nome'/>
+            <CampoTexto obrigatorio={true} label="Classe" placeholder='Digite seu classe'/>
             <CampoTexto label="Imagem" placeholder='Digite seu endereço da imagem'/>
-            <ListaSuspensa label="Time" itens={times} />
+            <ListaSuspensa obrigatorio={true} label="Time" itens={times} />
             <Botao > 
                 Criar card
             </Botao >
