9056a309c8d8b4be5a383479f194bb475d4b8cca
Alan Alves <alan.alveses@gmail.com>
2024-01-19 22:38:09 -0300

    Manipulado estados com useState

--- src/componentes/Formulario/index.js
+++ src/componentes/Formulario/index.js
@@ -1,3 +1,4 @@
+import { useState } from 'react'
 import Botao from '../Botao'
 import CampoTexto from '../CampoTexto'
 import ListaSuspensa from '../ListaSuspensa'
@@ -14,18 +15,46 @@ const Formulario = () => {
         'Soldado'
     ]
 
+    const [nome, setNome] = useState('')
+    const [classe, setClasse] = useState('')
+    const [imagem, setImagem] = useState('')
+    const [time, setTime] = useState('')
+
     const aoSalvar = (evento) => {
         evento.preventDefault()
-        console.log('Form foi enviado')
+        console.log('Form foi enviado =>', nome, classe, imagem, time)
     }
     return(
         <section className='formulario'>
             <form onSubmit={aoSalvar}>
                 <h2>Preencha os dados para criar o card dos jogadores</h2>
-            <CampoTexto obrigatorio={true} label="Nome" placeholder='Digite seu nome'/>
-            <CampoTexto obrigatorio={true} label="Classe" placeholder='Digite seu classe'/>
-            <CampoTexto label="Imagem" placeholder='Digite seu endereço da imagem'/>
-            <ListaSuspensa obrigatorio={true} label="Time" itens={times} />
+            <CampoTexto 
+            obrigatorio={true} 
+            label="Nome" 
+            placeholder='Digite seu nome'
+            valor={nome}
+            aoAlterado={valor => setNome(valor)}
+            />
+            <CampoTexto 
+            obrigatorio={true} 
+            label="Classe" 
+            placeholder='Digite seu classe'
+            valor={classe}
+            aoAlterado={valor => setClasse(valor)}
+            />
+            <CampoTexto 
+            label="Avatar" 
+            placeholder='Digite seu endereço da imagem'
+            valor={imagem}
+            aoAlterado={valor => setImagem(valor)}
+            />
+            <ListaSuspensa 
+            obrigatorio={true} 
+            label="Time" 
+            itens={times} 
+            valor={time}
+            aoAlterado={valor => setTime(valor)}
+            />
             <Botao > 
                 Criar card
             </Botao >
