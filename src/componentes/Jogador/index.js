26abe231c4fc9f3c641d7d9bb2bc230aad9aaf01
Alan Alves <alan.alveses@gmail.com>
2024-01-21 10:58:05 -0300

    Criando o componente "Jogador"

---
+++ src/componentes/Jogador/index.js
@@ -0,0 +1,17 @@
+import './Jogador.css'
+
+const Jogador = () => {
+    return (
+        <div className='jogador'>
+            <div className='cabecalho'>
+                <img src='https://github.com/alanalveses.png' alt='' />
+            </div>
+            <div className='rodape'>
+                <h4>Alan Alves </h4>
+                <h5>Tank </h5>
+            </div>
+        </div>
+    )
+}
+
+export default Jogador
--- src/componentes/Time/index.js
+++ src/componentes/Time/index.js
@@ -1,3 +1,4 @@
+import Jogador from '../Jogador'
 import './Time.css'
 
 const Time = (props) => {
@@ -7,6 +8,8 @@ const Time = (props) => {
     return (
         <section className='time' style={{ backgroundColor: props.corSecundaria}}>
             <h3 style={{borderColor: props.corPrimaria}}>{props.nome}</h3>
+            <Jogador />
+            <Jogador />
         </section>
     )
 }
